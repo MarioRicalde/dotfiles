da122f3e83aece5a0c4abddc41a8ec5071f5d8b2
MarioRicalde <kuroir@gmail.com>
2021-03-19 07:49:32 -0600

    feat: default editor is now vim (nvim)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,7 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow'
 export FZF_CTRL_T_COMMAND='fd --type d --hidden --follow'
 
 export XDG_DATA_HOME='~/.config'
+
+export vim=nvim
+export VISUAL=vim
+export EDITOR="$VISUAL"
